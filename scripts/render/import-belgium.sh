f68414da4e62a2ca6aac4d08535324ab3c33105f
Jonathan Beliën <jbe@geo6.be>
2018-04-17 10:17:08 +0200

    Update import-belgium.sh

--- scripts/render/import-belgium.sh
+++ scripts/render/import-belgium.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://download.geofabrik.de/europe/belgium-latest.osm.pbf && \
+wget https://download.geofabrik.de/europe/belgium-latest.osm.pbf -O ~/data/belgium-latest.osm.pbf && \
 osm2pgsql --database gis --slim --cache 23040 --number-processes 8 --multi-geometry --hstore \
   --style ~/src/openstreetmap-carto-be/openstreetmap-carto.style \
   --tag-transform-script ~/src/openstreetmap-carto-be/openstreetmap-carto.lua \
