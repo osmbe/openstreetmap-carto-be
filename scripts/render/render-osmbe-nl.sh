b438bdfab382e6073e51f6be92eb5d18e38c546c
Jonathan Beliën <jbe@geo6.be>
2018-04-18 11:24:50 +0200

    Update update-repo.sh

--- scripts/render/render-osmbe-nl.sh
+++ scripts/render/render-osmbe-nl.sh
@@ -9,6 +9,6 @@ rm -r /var/lib/mod_tile/osmbe-nl/*
 
 render_list -m osmbe-nl -n 8 -z 0 -Z 7 --all
 
-~/src/render_list_geo.pl -m osmbe-nl -n 8 -z 8 -Z 18 -x 2.5 -X 6.5 -y 49.4 -Y 51.6
+~/src/render_list_geo.pl/render_list_geo.pl -m osmbe-nl -n 8 -z 8 -Z 18 -x 2.5 -X 6.5 -y 49.4 -Y 51.6
 
 rsync -avz --delete /var/lib/mod_tile/osmbe-nl/ ubuntu@54.38.71.178:/mnt/osmbe-nl/ --progress
--- scripts/render/update-repo.sh
+++ scripts/render/update-repo.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-wget https://github.com/alx77/render_list_geo.pl/blob/master/render_list_geo.pl -O ~/src/render_list_geo.pl
+cd ~/src/render_list_geo.pl && \
+git fetch --all && \
+git reset --hard origin/master && \
+git pull origin master
 
 cd ~/src/openstreetmap-carto-be && \
 git fetch --all && \
