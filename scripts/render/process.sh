0954ddd13c707d663434f9553feeabf2319ada94
Jonathan Beliën <dev@jbelien.be>
2018-11-16 13:53:16 +0100

    Update process.sh

--- scripts/render/process.sh
+++ scripts/render/process.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-# Update "openstreetmap-carto-be" and "render_list_geo.pl"
-sh update-repo.sh
+# Screen colors using tput
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+RESET=`tput sgr0`
 
-#
-sudo mkdir -p /var/run/renderd && \
-sudo chown $USER /var/run/renderd
+# Update "openstreetmap-carto-be" and "render_list_geo.pl"
+echo "${YELLOW}Update repositories${RESET}"
+sh update-repo.sh
 
 # Render
 if [ $# -eq 0 ]; then
-    sh render-osmbe.sh
+    echo "${YELLOW}Launch render: \"osmbe\"${RESET}"
+    sh "render-osmbe.sh"
 elif [ -f "render-osmbe-$1.sh" ]; then
+    echo "${YELLOW}Launch render: \"osmbe-$1\"${RESET}"
     sh "render-osmbe-$1.sh"
 else
-    echo "Version \"$1\" not available !"
+    echo "${RED}Version \"$1\" not available !${RESET}"
 fi
